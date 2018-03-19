bf52e863a070015f2875aab337fe77062dcc37d7
Alex Keivahn Smith <Alex@Alexs-MBP-2.fios-router.home>
2018-03-18 20:40:44 -0400

    3rd try

--- static/js/scrolltransition.js
+++ static/js/scrolltransition.js
@@ -21,27 +21,28 @@ $(document).ready(function(){
     }
 });
 
+// disabled
 // upon scrolling fade in the appropriate divs
-$(document).ready(function() {
-
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-
-        /* Check the location of each desired element */
-        $('.fadein').each( function(i){
-
-            var bottom_of_object = $(this).position().top; // optional: add "+ $(this).outerHeight()" if you want to fade in only when the whole div is visible
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-
-                $(this).animate({'opacity':'1'},1500);
-
-            }
-        });
-    });
-});
+// $(document).ready(function() {
+//
+//     /* Every time the window is scrolled ... */
+//     $(window).scroll( function(){
+//
+//         /* Check the location of each desired element */
+//         $('.fadein').each( function(i){
+//
+//             var bottom_of_object = $(this).position().top; // optional: add "+ $(this).outerHeight()" if you want to fade in only when the whole div is visible
+//             var bottom_of_window = $(window).scrollTop() + $(window).height();
+//
+//             /* If the object is completely visible in the window, fade it it */
+//             if( bottom_of_window > bottom_of_object ){
+//
+//                 $(this).animate({'opacity':'1'},1500);
+//
+//             }
+//         });
+//     });
+// });
 
 // upon scrolling, hide the downarrow when reaching the footer
 $(document).ready(function(){
